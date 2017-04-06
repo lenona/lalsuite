5b65dcacfe9697c89e25cdae685f32d9fddc4278
Amber Lenon <amber.lenon@ligo.org>
2017-04-06 10:21:01 -0400

    fix print statements

--- lalsimulation/src/inspiral.c
+++ lalsimulation/src/inspiral.c
@@ -510,11 +510,11 @@ int create_fd_waveform(COMPLEX16FrequencySeries ** htilde_plus, COMPLEX16Frequen
     if (p.condition) {
         if (p.verbose) {
             fprintf(stderr, "generating waveform in frequency domain using XLALSimInspiralFD...\n");
-	    fprintf("here1\n"); 
+	    fprintf(stderr,"here1\n"); 
             timer_start = clock();
-		fprintf("here2\n");
+		fprintf(stderr,"here2\n");
         }
-	fprintf("here3\n");
+	fprintf(stderr,"here3\n");
         XLALSimInspiralFD(htilde_plus, htilde_cross, p.m1, p.m2, p.s1x, p.s1y, p.s1z, p.s2x, p.s2y, p.s2z, p.distance, p.inclination, p.phiRef, p.longAscNodes, p.eccentricity, p.meanPerAno, deltaF, p.f_min, 0.5 * p.srate, p.fRef, p.params, p.approx);
         if (p.verbose)
             fprintf(stderr, "generation took %g seconds\n", (double)(clock() - timer_start) / CLOCKS_PER_SEC);
